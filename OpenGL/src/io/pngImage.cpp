11be0994aabc5817419594fd98725d949286b6dd
千浔 <xtmacbook@users.noreply.github.com>
2020-11-06 19:19:33 +0800

    Update pngImage.cpp

--- OpenGL/src/io/pngImage.cpp
+++ OpenGL/src/io/pngImage.cpp
@@ -3,11 +3,12 @@
 #include "image.h"
 #include "gls.h"
 #include "log.h"
+#include "engineLoad.h"
 
 #ifdef ENGINE_PNG
 #include <png.h>
 #endif
-
+#include <vector>
 namespace IO
 {
 #if defined(ENGINE_PNG)
@@ -27,189 +28,88 @@ namespace IO
 	}
 #endif
 
-	bool ImageFile::loadPNG(StdInputStream * stream, base::SmartPointer<base::Image>&img)
+	base::Image* ImageFile::loadPNG(StdInputStream * stream, const ImageOption*option)
 	{
+		png_image image_png;
+		memset(&image_png, 0, sizeof(image_png));
+		image_png.version = PNG_IMAGE_VERSION;
+		image_png.opaque = nullptr;
 
-#if defined(ENGINE_PNG)
-		png_structp png_ptr;
-		png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-		if (png_ptr == NULL)
+		std::vector<int8> byte_array;
+		byte_array.resize(stream->size());
+		stream->serializeData(byte_array.data(), stream->size());
+
+		if (!png_image_begin_read_from_memory(&image_png, (png_const_voidp)byte_array.data(), stream->size()))
+
+			return nullptr;
+
+		size_t size = PNG_IMAGE_SIZE(image_png); // get image size
+
+		base::Image * image = new base::Image;
+		image->allocate(size);
+		image->levelDataPtr_ = new uint8*[1];
+		image->levelDataPtr_[0] = (uint8*)image->pixels();
+
+		if (png_image_finish_read(&image_png, NULL/*background*/, image->pixels(), 0/*row_stride*/, nullptr/*colormap*/))
 		{
-			Log::instance()->printMessage("*** LoadPNG: Error allocating read buffer in file '%s'.\n");
-			return false;
-		}
+			size_t pixel_size = 0;
 
-		// Allocate/initialize a memory block for the image information
-		png_infop info_ptr = png_create_info_struct(png_ptr);
-		if (info_ptr == NULL)
-		{
-			png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
-			Log::instance()->printMessage("*** LoadPNG: Error allocating image information for '%s'.\n");
-			return false;
-		}
+			image->setwidth(image_png.width);
+			image->setheight(image_png.height);
 
-		// Set up the error handling
-		if (setjmp(png_jmpbuf(png_ptr)))
-		{
-			png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
-			Log::instance()->printMessage("*** LoadPNG: Error reading png file '%s'.\n");
-			return false;
-		}
-
-		// Set up the I/O functions.
-		png_set_read_fn(png_ptr, stream, user_read_data);
-
-		// Retrieve the image header information
-		png_uint_32 width, height;
-		int bit_depth, color_type, interlace_type;
-		png_read_info(png_ptr, info_ptr);
-		png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type, &interlace_type, NULL, NULL);
-
-
-		if (color_type == PNG_COLOR_TYPE_PALETTE && bit_depth <= 8)
-		{
-			// Convert indexed images to RGB.
-			png_set_expand(png_ptr);
-		}
-		else if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
-		{
-			// Convert grayscale to RGB.
-			png_set_expand(png_ptr);
-		}
-		else if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
-		{
-			// Expand images with transparency to full alpha channels
-			// so the data will be available as RGBA quartets.
-			png_set_expand(png_ptr);
-		}
-		else if (bit_depth < 8)
-		{
-			// If we have < 8 scale it up to 8.
-			//png_set_expand(png_ptr);
-			png_set_packing(png_ptr);
-		}
-
-		// Reduce bit depth.
-		if (bit_depth == 16)
-		{
-			png_set_strip_16(png_ptr);
-		}
-
-		// Represent gray as RGB
-		if (color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
-		{
-			png_set_gray_to_rgb(png_ptr);
-		}
-
-		// Convert to RGBA filling alpha with 0xFF.
-		if (!(color_type & PNG_COLOR_MASK_ALPHA))
-		{
-			png_set_filler(png_ptr, 0xFF, PNG_FILLER_AFTER);
-		}
-
-		// @todo Choose gamma according to the platform?
-		double screen_gamma = 2.2;
-		int intent;
-		if (png_get_sRGB(png_ptr, info_ptr, &intent)) {
-			png_set_gamma(png_ptr, screen_gamma, 0.45455);
-		}
-		else {
-			double image_gamma;
-			if (png_get_gAMA(png_ptr, info_ptr, &image_gamma)) {
-				png_set_gamma(png_ptr, screen_gamma, image_gamma);
+			// check file format
+			switch (image_png.format)
+			{
+			case PNG_FORMAT_GRAY:
+				pixel_size = 1;
+				image->setComponents(1);
+				break;
+			case PNG_FORMAT_LINEAR_Y:
+				pixel_size = 2;
+				image->setComponents(1);
+				image->settarget(GL_TEXTURE_2D);
+				if (!option->clamped_)
+					image->setinternalformat(GL_R16F);
+				else
+					image->setinternalformat(GL_R16);
+				image->settype(GL_SHORT);
+				image->setformat(GL_RED);
+				break;
+			case PNG_FORMAT_RGB:
+				pixel_size = 3;
+				image->setComponents(3);
+				image->settarget(GL_TEXTURE_2D);
+				image->setinternalformat(GL_RGB8);
+				image->settype(GL_UNSIGNED_BYTE);
+				image->setformat(GL_RGB);
+				break;
+			case PNG_FORMAT_RGBA:
+				pixel_size = 4;
+				image->setComponents(4);
+				image->settarget(GL_TEXTURE_2D);
+				image->setinternalformat(GL_RGBA8);
+				image->settype(GL_UNSIGNED_BYTE);
+				image->setformat(GL_RGBA);
+				break;
+			default:
+			{
+				printf("undefine png format!\n");
+				delete image;
+				return nullptr;
 			}
-			else {
-				png_set_gamma(png_ptr, screen_gamma, 0.45455);
 			}
+
+			image->settypesize(0);
+			image->setfaces(1);
+			image->setElementSize(pixel_size);
+			png_image_free(&image_png);
+			image->setdepth(1);
+
+			return image;
+
 		}
+		return nullptr;
 
-		// Perform the selected transforms.
-		png_read_update_info(png_ptr, info_ptr);
-
-		png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type, &interlace_type, NULL, NULL);
-
-		if (bit_depth != 8)
-		{
-			png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-			Log::printMessage("this png bit depth is not equal 8 !");
-			return false;
-		}
-
-		// Set internal format flags.
-		if (color_type & PNG_COLOR_TYPE_RGB_ALPHA)
-		{
-			img->setformat(GL_RGBA);
-			img->setinternalformat(GL_RGBA8);
-		}
-		else if (color_type & PNG_COLOR_TYPE_RGB)
-		{
-			img->setformat(GL_RGB);
-			img->setinternalformat(GL_RGB8);
-		}
-		else if (color_type & PNG_COLOR_TYPE_GRAY)
-		{
-			png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-			//img->setformat(); gray
-			Log::printMessage("this png color type is gray!");
-			return false;
-		}
-
-		int rowbytes = png_get_rowbytes(png_ptr, info_ptr);
-		// glTexImage2d requires rows to be 4-byte aligned
-		rowbytes += 3 - ((rowbytes - 1) % 4);
-
-		// Row size in bytes.
-		int channel = rowbytes / width;
-		img->allocate(width, height, channel);
-
-		img->levelDataPtr_ = new uint8*[1];
-		img->levelDataPtr_[0] = (uint8*)img->pixels();
-
-		// Read the image
-		uint8 * pixels = (uint8 *)img->pixels();
-		png_bytep * row_data = new png_bytep[sizeof(png_byte) * height];
-
-		for (uint i = 0; i < height; i++)
-		{
-			row_data[i] = &(pixels[width * channel * i]);
-		}
-
-		png_read_image(png_ptr, row_data);
-
-		// Finish things up
-		png_read_end(png_ptr, info_ptr);
-		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-
-		//// RGBA to BGRA.
-		//uint num = width * height;
-		//for (uint i = 0; i < num; i++)
-		//{
-		//	uint8 r = ((uint8*)img->pixels())[i * img->depth()];
-		//	uint8 g = ((uint8*)img->pixels())[i * img->depth() + 1];
-		//	uint8 b = ((uint8*)img->pixels())[i * img->depth() + 2];
-		//	uint8 a = ((uint8*)img->pixels())[i * img->depth() + 3];
-
-		//	((int8*)img->pixels())[i * img->depth()] = b;
-		//	((uint8*)img->pixels())[i * img->depth() + 1] = g;
-		//	((uint8*)img->pixels())[i * img->depth() + 2] = r;
-		//	((uint8*)img->pixels())[i * img->depth() + 3] = a;
-		//}
-
-		// Compute alpha channel if needed.
-		/*if( img->flags & PI_IU_BUMPMAP || img->flags & PI_IU_ALPHAMAP ) {
-		if( img->flags & PI_IF_HAS_COLOR && !(img->flags & PI_IF_HAS_ALPHA)) {
-		img->ComputeAlphaFromColor();
-		}
-		}*/
-
-		img->settarget(GL_TEXTURE_2D);
-		img->settype(GL_UNSIGNED_BYTE);
-		img->settypesize(0);
-		img->setfaces(1);
-
-		delete[] row_data;
-		return true;
-#endif
-		return false;
 	}
+
 }
