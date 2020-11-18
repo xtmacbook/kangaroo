6efd470a349bb7794880ca178020ec63cf165e21
千浔 <xtmacbook@users.noreply.github.com>
2020-11-18 17:15:40 +0800

    Update common.h

--- OpenGL/include/common.h
+++ OpenGL/include/common.h
@@ -14,15 +14,17 @@ unsigned int											stringToUint(const std::string &source);
 int														stringToInt(const std::string &source);
 float													stringToFloat(const std::string &source);
 int														strCaseDiff(const char * s1, const char * s2);
+std::wstring										    utf8toUtf16(const std::string&);
 
-template<class T> inline std::string					toString(const T& input)
+const char *											extension(const char * str);
+
+template<class T> inline std::string					
+	toString(const T& input)
 {
 	std::ostringstream converter;
 	converter << input;
 	return converter.str();
 }
-
-const char *						extension(const char * str);
 
 template <typename T>
 inline void swap(T & a, T & b)
