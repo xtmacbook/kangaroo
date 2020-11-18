581167b58d776d56becb20b75ea3264c1c542d29
千浔 <xtmacbook@users.noreply.github.com>
2020-11-18 17:16:45 +0800

    Update framebuffers.h

--- OpenGL/include/framebuffers.h
+++ OpenGL/include/framebuffers.h
@@ -4,120 +4,77 @@
 
 #include "decl.h"
 #include "BaseObject.h"
+#include "util.h"
 
-typedef enum {
-	FBO_DepthBufferType_NONE = 0,
-	FBO_DepthBufferType_RENDERTARGET = 1,						//24bits depth buffer
-	FBO_DepthBufferType_TEXTURE = 2,							//24bits depth buffer
-	FBO_DepthBufferType_TEXTURE_PACKED_DEPTH24_STENCIL8 = 3,	//24bits depth buffer + packed 8bits stencil (32bits per pixel)
-	FBO_DepthBufferType_TEXTURE_DEPTH32F = 4,					//32bits floating point depth buffer
-	FBO_DepthBufferType_TEXTURE_PACKED_DEPTH32F_STENCIL8 = 5,	//32bits floating point depth buffer + packed 8bits stencil (64bits per pixel)
-	FBO_DepthBufferType_TEXTURE_FROM_ANOTHER_FBO = 6			//link to this FBO the same dept buffer as another FBO passed in as a parameter
-} FBO_DepthBufferType;
+#include <vector>
 
 class Texture;
 
-class FrameBufferObject  :public base::BaseObject
+/*
+	Attach :Attachment is different from binding. Objects are bound to the context; objects are attached to one another.
+
+	Attachment point:  location within a framebuffer object hat a framebuffer-attachable image or layered image can be attached to.
+*/
+
+class LIBENIGHT_EXPORT FrameBufferObject  :public base::BaseObject
 {
 
-public:
-
-	enum FrameType
+	enum Type
 	{
-		CFI,
-		CO
+		COLOR_ONLY = 0x0001,
+		DEPTH_ONLY = 0x0002,
+		COLOR_DEPTH = 0x0003
 	};
 
-	FrameBufferObject(unsigned int target,unsigned int width, unsigned int height,
-		unsigned int nbColorBuffer,
-		const unsigned int* colorBufferInternalFormat,
-		const unsigned int* colorBufferSWRAP,
-		const unsigned int* colorBufferTWRAP,
-		const unsigned int* colorBufferMinFiltering,
-		const unsigned int* colorBufferMagFiltering,
-		FBO_DepthBufferType depthBufferType,
-		const unsigned int depthBufferMinFiltering,
-		const unsigned int depthBufferMagFiltering,
-		const unsigned int depthBufferSWRAP,
-		const unsigned int depthBufferTWRAP,
-		bool depthTextureCompareToR ,
-		FrameBufferObject* fboContainingDepthBuffer);
+	enum DepthType
+	{
+		DEPTH,
+		STENCIL,
+		DEPTH_STENCIL
+	};
 
-	FrameBufferObject(Texture * colorTextures, unsigned int nbColorBuffer);
+	ENGINE_FORBID_COPY(FrameBufferObject)
 
-	~FrameBufferObject();
+	FrameBufferObject(unsigned int target);
 
-	FrameBufferObject();
-	
-	void		colorTextureAttachments(Texture *, unsigned int nColorBuffer);
+	virtual ~FrameBufferObject();
 
-	unsigned int getNumberOfColorAttachement() const;
-	
-	FBO_DepthBufferType getDepthBufferType() const;
-	unsigned int getWidth() const;
-	unsigned int getHeight() const;
-	static const int getMaxColorAttachments();
-	
-	static const int getMaxBufferSize();
-	
-	void enableRenderToColorAndDepth(const unsigned int colorBufferNum) const;
-	
-	void FrameBufferObject::enableRenderToColorAndDepth_MRT(const unsigned int numBuffers, const  unsigned int* drawbuffers) const;
-	 
-	static void disableRenderToColorDepth();
+	void		colorTextureAttachments(Texture * color);
+	void		colorTextureAttachments(std::vector<Texture *>color);
+	void		depthTextureAttachments(Texture*depth, DepthType);
+	void		colorAndDepthTextureAttachments(std::vector<Texture *>color, Texture*depth, DepthType);
 
-	 
-	void saveAndSetViewPort() const;
-	 
-	void restoreViewPort() const;
+	static const int	getMaxColorAttachments();
 
-	 
-	void bindColorTexture(const unsigned int colorBufferNum) const;
-	 
-	void bindDepthTexture() const;
+	static const int	getMaxRenderBufferSize()  ;
 
-	 
-	void generateColorBufferMipMap(const unsigned int colorBufferNum) const;
-	 
-	void generateDepthBufferMipMap() const;
-	 
-	bool isColorMipMapPyramidValid(const unsigned int colorBufferNum) const;
-	 
-	bool isDepthMipMapPyramidValid() const;
-	void saveToDisk(const unsigned int colorBufferNum, const char* filepath) const;
+	void				bindObj(bool bind = true,bool flag = true);
 
-	bool isFrameBufferComplete();
+	void			clearBuffer();
+private:
+
+	void				createGLObject();
+	void				destoryGLObject();
+
+	bool				checkFramebufferStatus();
+	bool				hasCreateObj_ = false;
 
 private:
-	void  colorBuffer(unsigned int target, unsigned int width, unsigned int height,
-		const unsigned int* colorBufferInternalFormat, const unsigned int* colorBufferSWRAP,
-		const unsigned int* colorBufferTWRAP, const unsigned int* colorBufferMinFiltering,
-		const unsigned int* colorBufferMagFiltering);
-	void  depthBuffer(FBO_DepthBufferType depthBufferType,
-		const unsigned int depthBufferMinFiltering, const unsigned int depthBufferMagFiltering,
-		const unsigned int depthBufferSWRAP, const unsigned int depthBufferTWRAP, bool depthTextureCompareToR);
-	void  framebuffer(unsigned int nbColorBuffer, FrameBufferObject* fboContainingDepthBuffer,bool bindDefault = true);
 
-	private:
+	//glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT + i, this->target_, this->colorTextures_[i], 0);
 
-		unsigned int fbo_;
-		unsigned int* colorTextures_ = nullptr;
-		int nbColorAttachement_;
-		unsigned int *colorMinificationFiltering_ = nullptr;
-		unsigned int depthID_;
-		FBO_DepthBufferType depthType_;
-		unsigned int depthMinificationFiltering_;
-		unsigned int width_;
-		unsigned int height_;
-		unsigned int target_;
+	std::vector<Texture*>		colorTextures_;
+	Texture*					depthTexture_ = nullptr;
 
-		mutable bool* validColorMipMapPyramid_ = nullptr;
-		mutable bool validDepthMipMapPyramid_;
+	unsigned int	fboObj_;
 
-		FrameBufferObject(FrameBufferObject&);
-		FrameBufferObject& operator=(const FrameBufferObject&);
+	unsigned int				ncolorBuffer_;
 
-		FrameType  type_;
+	unsigned int	width_;
+	unsigned int	height_;
+	unsigned int	target_;
+
+	Type			type_ = COLOR_DEPTH;
 
 };
 
