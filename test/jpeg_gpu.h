a6de041d00e0da9e1885c9b096836fac2d72ff47
千浔 <xtmacbook@users.noreply.github.com>
2020-11-18 17:26:45 +0800

    Update jpeg_gpu.h

--- test/jpeg_gpu.h
+++ test/jpeg_gpu.h
@@ -1,16 +1,42 @@
 
 
 #include "smartPointer.h"
+#include <vector>
 
 class Texture;
+class Shader;
 class FrameBufferObject;
 
 #define MAX_CHANELS 3
 
-class JPEG_GPU
+class UpdateInfo
 {
+	int     blocksNum;      // A total number of blocks reserved for a single update step.
+							// This should be at least StackSize / UpdateBorderSize
+	int     curBlock;
+	int     blocksAdded;
+	int     *pSrcCorners;   // Values used to map data during UpdateSubresource calls
+							// when we prepare data for IDCT pass.
+							// Here we store only the positions of a left upper corner,
+							// because update region size depends on the resource being used.
+	int     *pDstCorners;   // Values used to map data during CopySubresourceRegion calls
+							// when we update our clipmap stack texture with uncompressed data.
+
 public:
 
+	UpdateInfo();
+	~UpdateInfo();
+	void    initialize(int blocks);
+	void    addBlock(int DstBlock[2], int SrcBlock[2]);
+	int     getNewBlocksNumber() { return blocksAdded; }
+	int*    getSrcBlocksPointer() { return pSrcCorners; }
+	int*    getDstBlocksPointer() { return pDstCorners; }
+	void    reset();
+	void    release();
+};
+
+struct GPU_Data
+{
 	base::SmartPointer<Texture> pTextureDCT;
 	base::SmartPointer<Texture> pTextureQ;
 
@@ -22,25 +48,42 @@ public:
 	base::SmartPointer<Texture>                pTexture1Col;
 	base::SmartPointer<Texture>                pTexture2Col;
 
-	base::SmartPointer<FrameBufferObject>	row1FrameBuffer_;
-	base::SmartPointer<FrameBufferObject>	row2FrameBuffer_;
-	base::SmartPointer<FrameBufferObject>	col1FrameBuffer_;
-	base::SmartPointer<FrameBufferObject>	col2FrameBuffer_;
+	base::SmartPointer<FrameBufferObject>	rowFrameBuffer_;
+	base::SmartPointer<FrameBufferObject>	colFrameBuffer_;
 
 	// Final texture
 	base::SmartPointer<Texture>                pTextureTarget;
-
-
-	JPEG_GPU();
-
 
 	int intitialize(int width, int height, char *pQuantTable);
 	void release();
 };
 
-
 class Jpeg_Data
 {
+
+public:
+
+	Jpeg_Data(const char*file);
+
+	Jpeg_Data();
+
+	virtual ~Jpeg_Data();
+
+	int			allocateTextures(int width, int height);
+
+	void		updateTextureData(int blocksNum, int blockSize, int *pSrcCorners);
+
+	void		uncompressTextureData();
+
+	bool		loadFile(const char*file);
+
+	int			height()const;
+	int			width()const;
+
+	static	bool	initTechnique();
+
+	static  Shader*	 getTechnique(const char*);
+	
 protected:
 	int     imageWidth;
 	int     imageHeight;
@@ -51,12 +94,14 @@ protected:
 	int     MCU_per_row;
 	int     MCU_blocks_num;
 
-	char    *pFileData;
+	char*	pFileData = nullptr;
+	void*	mappingHandle = nullptr;
+	void*	fileHandle = nullptr;
 
 	int      format;
 	int		 type;
-public:
-	bool		loadFile(const char*file);
+	GPU_Data	textureData[MAX_CHANELS];
 
-	JPEG_GPU	textureData[MAX_CHANELS];
+	static std::vector< base::SmartPointer<Shader> > shaders_;
 };
+
