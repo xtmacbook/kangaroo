1603024c5197951c8938fa06171cdef5c9ed8fb0
千浔 <xtmacbook@users.noreply.github.com>
2020-11-20 18:14:27 +0800

    Update clipmap_prepprocessor.cpp

---
+++ test/clipmap_prepprocessor.cpp
@@ -0,0 +1,200 @@
+
+#include "clipmap_prepprocessor.h"
+#include "debug.h"
+#include "util.h"
+#include "jpgd.h"
+#include "resource.h"
+#include "texture.h"
+#include "gls.h"
+#include "log.h"
+#include "framebuffers.h"
+#include "common.h"
+#include "shader.h"
+
+#include <algorithm>
+
+Clipmap_Manager::Clipmap_Manager()
+{
+	isInitialized = false;
+	levelsNum = 0;
+	pimgData = nullptr;
+	pUpdateBlocks = nullptr;
+	ppBakedTextures = nullptr;
+	ppFrameBuffers = nullptr;
+}
+
+Clipmap_Manager::~Clipmap_Manager()
+{
+	SAFTE_DELETE(pimgData)
+	SAFTE_DELETE_ARRAY(pimgDataHM)
+	SAFTE_DELETE(pUpdateBlocks)
+
+	if(ppBakedTextures)
+	{
+		for (int i = 0; i < levelsNum; i++)
+		{
+			SAFTE_DELETE(ppBakedTextures[i])
+		}
+		SAFTE_DELETE_ARRAY(ppBakedTextures);
+	}
+	if (ppFrameBuffers)
+	{
+		for (int i = 0; i < levelsNum; i++)
+		{
+			SAFTE_DELETE(ppFrameBuffers[i])
+		}
+		SAFTE_DELETE_ARRAY(ppFrameBuffers);
+	}
+}
+
+bool Clipmap_Manager::intitialize(int levels, char fileNames[][260], char fileNamesHM[][260])
+{
+	if (!isInitialized)
+	{
+		if (!levels)
+			return false;
+
+		levelsNum = levels;
+
+		pimgData = new Image_Data[levelsNum];
+		pUpdateBlocks = new UpdateInfo[levelsNum];
+
+		if (fileNamesHM)
+		{
+			pimgDataHM = new Image_Data[levelsNum];
+		}
+
+		for (int i = 0; i < levelsNum; i++)
+		{
+			enAssert(pimgData[i].loadFile((get_texture_BasePath() + fileNames[i]).c_str()));
+
+			if (fileNamesHM)
+				enAssert(pimgDataHM[i].loadFile( (get_texture_BasePath() + fileNamesHM[i]).c_str()));
+
+		}
+
+		isInitialized = true;
+
+		Image_Data::initTechnique();
+	}
+
+	return true;
+}
+
+void Clipmap_Manager::addBlock(int level, int srcBlock[2], int dstBlock[2])
+{
+	if (level < levelsNum)
+	{
+		pUpdateBlocks[level].addBlock(srcBlock, dstBlock);
+	}
+}
+
+void Clipmap_Manager::allocateBlocks(int blocksNum)
+{
+	for (int i = 0; i < levelsNum; i++)
+	{
+		pUpdateBlocks[i].initialize(int(blocksNum * pow(2.0, i)));
+	}
+}
+
+void Clipmap_Manager::update(int level, base::SmartPointer<Texture> stackTexture)
+{
+	GLint viewport[4];
+	glGetIntegerv(GL_VIEWPORT, viewport);
+	if (level < levelsNum)
+	{
+		int blocksNum = pUpdateBlocks[level].getNewBlocksNumber();
+
+		// Process diffuse texture
+		pimgData[level].updateTextureData( blocksNum, blockSize >> level, pUpdateBlocks[level].getSrcBlocksPointer());
+		pimgData[level].uncompressTextureData();
+		pUpdateBlocks[level].reset();
+
+		ppFrameBuffers[level]->bindObj(true, true);
+		ppFrameBuffers[level]->clearBuffer();
+		glViewport(0, 0, ppBakedTextures[level]->width(), ppBakedTextures[level]->heigh());
+		Shader * shader = Jpeg_Data::getTechnique("IDCT_RenderToBuffer");
+		shader->turnOn();
+		glActiveTexture(GL_TEXTURE0);
+		pimgData[level].getFinalTarget(0)->bind();
+		glActiveTexture(GL_TEXTURE1);
+		pimgData[level].getFinalTarget(1)->bind();
+		glActiveTexture(GL_TEXTURE2);
+		pimgData[level].getFinalTarget(2)->bind();
+		Jpeg_Data::getQuad()->draw();
+		ppFrameBuffers[level]->bindObj(false, true);
+
+		//copy texture to stacktexture
+
+		int *pDstBlocks = pUpdateBlocks[level].getDstBlocksPointer();
+		int levelBlockSize = blockSize >> level;
+
+		int srcLeft = 0;
+		int srcBottom = 0;
+
+		// Copy fully unpacked data to the clipmap stack texture
+		for (int i = 0; i < blocksNum; i++)
+		{
+			
+			glCopyImageSubData(ppBakedTextures[level]->getTexture(),//GLuint srcName,
+				GL_TEXTURE_2D,//GLenum srcTarget,
+				0,//GLint srcLevel,
+				srcLeft,//GLint srcX,
+				srcBottom,//GLint srcY,
+				0,//GLint srcZ,
+				stackTexture->getTexture(),//GLuint dstName,
+				GL_TEXTURE_2D_ARRAY,//GLenum dstTarget,
+				0,//GLint dstLevel,
+				pDstBlocks[i * 2],//GLint dstX,
+				pDstBlocks[i * 2 + 1],//GLint dstY,
+				level,
+				levelBlockSize,//GLsizei srcWidth,
+				levelBlockSize,//GLsizei srcHeight,
+				0);//GLsizei srcDepth
+			 
+			srcLeft += levelBlockSize;
+			srcBottom += levelBlockSize;
+		}
+	}
+	glViewport(viewport[0], viewport[1], viewport[2], viewport[3]);
+}
+
+int Clipmap_Manager::allocateTextures(int stackSize, int border)
+{
+
+	textureSize = stackSize;
+	blockSize = border;
+
+	ppBakedTextures = new Texture*[levelsNum];
+	ppFrameBuffers = new FrameBufferObject*[levelsNum];
+
+	for (int i = 0; i < levelsNum; i++)
+	{
+		enAssert((pimgData[i].allocateTextures(stackSize, border)));
+		//enAssert((pimgDataHM[i].allocateTextures(stackSize, border)));
+
+		ppBakedTextures[i] = new Texture;
+		ppBakedTextures[i]->width_ = stackSize;
+		ppBakedTextures[i]->height_ = border;
+		ppBakedTextures[i]->internalformat_ = GL_RGBA8;
+		ppBakedTextures[i]->createObj();
+		ppBakedTextures[i]->bind();
+		ppBakedTextures[i]->mirrorRepeat();
+		ppBakedTextures[i]->filterLinear();
+		ppBakedTextures[i]->contextNULL();
+		ppBakedTextures[i]->unBind();
+
+		ppFrameBuffers[i] = new FrameBufferObject(GL_FRAMEBUFFER);
+		ppFrameBuffers[i]->bindObj(true, false);
+		ppFrameBuffers[i]->colorTextureAttachments(ppBakedTextures[i]);
+		ppFrameBuffers[i]->bindObj(false, false);
+		CHECK_GL_ERROR;
+
+		border >>= 1;
+	}
+
+	return 0;
+}
+
+
+ 
