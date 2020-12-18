4fa968f049eab4e36db41982da3695e7c4d0215d
千浔 <xtmacbook@users.noreply.github.com>
2020-12-18 17:27:35 +0800

    Create stateSet.h

---
+++ OpenGL/include/stateSet.h
@@ -0,0 +1,25 @@
+
+#ifndef __OpenGL__StateSet__
+#define __OpenGL__StateSet__
+
+#include "BaseObject.h"
+#include "smartPointer.h"
+
+class LIBENIGHT_EXPORT SampleObject :public base::BaseObject
+{
+public:
+
+	SampleObject();
+
+	virtual ~SampleObject();
+
+	void sampleParameteri(unsigned int pname, unsigned int val);
+
+	void bindTexture(unsigned int texture);
+
+private:
+	bool initGl();
+	unsigned int sampler_state_;
+};
+
+#endif
