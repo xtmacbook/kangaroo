47fd453be1d5bd381da56d6e53023326c2b58f3a
千浔 <xtmacbook@users.noreply.github.com>
2021-01-08 17:51:28 +0800

    Update dynamicMesh.cpp

--- OpenGL/src/geometry/dynamicMesh.cpp
+++ OpenGL/src/geometry/dynamicMesh.cpp
@@ -3,25 +3,21 @@
 
 #include <algorithm>
 
-DynamicMeshGeoemtry::DynamicMeshGeoemtry(int flag, unsigned verticesSize, unsigned indiceSize) :MeshGeometry(true),
-buffer_mode_(flag),
-vertices_buffer_pool_size_(verticesSize),
-indices_buffer_pool_size_(indiceSize)
+DynamicMeshGeoemtry::DynamicMeshGeoemtry(int flag, unsigned verticesSize, unsigned indiceSize, 
+	IRenderMeshObj_SP robj) :
+	MeshGeometry(robj, VERTEX_POINTS_TEXTURE),
+	buffer_mode_(flag),
+	vertices_buffer_pool_size_(verticesSize),
+	indices_buffer_pool_size_(indiceSize)
 {
 }
 
 DynamicMeshGeoemtry::~DynamicMeshGeoemtry()
 {
 	if (vertexBuffer_)
-	{
-		delete vertexBuffer_;
-		vertexBuffer_ = NULL;
-	}
+		SAFTE_DELETE(vertexBuffer_);
 	if (indicesBuffer_)
-	{
-		delete indicesBuffer_;
-		indicesBuffer_ = NULL;
-	}
+		SAFTE_DELETE(indicesBuffer_);
 	if (vao_)
 		glDeleteVertexArrays(1, &vao_);
 }
@@ -34,8 +30,7 @@ void DynamicMeshGeoemtry::postRender()
 
 void DynamicMeshGeoemtry::initGeometry()
 {
-	glGenVertexArrays(1, &vao_);
-	glBindVertexArray(vao_);
+	mesh_obj_->bind();
 
 	vertexBuffer_ = new FlashBuffer<Vertex>(vertices_buffer_pool_size_);
 	vertexBuffer_->setTarget(GL_ARRAY_BUFFER);
@@ -49,9 +44,10 @@ void DynamicMeshGeoemtry::initGeometry()
 	}
 
 	vertexBuffer_->bind();
-	setupVertexAttribute(RFVF());
+	setupVertexAttribute();
 	vertexBuffer_->unbind();
-	glBindVertexArray(0);
+	
+	mesh_obj_->unBind();
 }
 
 void DynamicMeshGeoemtry::updateGeometry(const CameraBase* camera)
@@ -59,7 +55,7 @@ void DynamicMeshGeoemtry::updateGeometry(const CameraBase* camera)
 	current_v_index_ = 0;
 	current_i_index_ = 0;
 
-	for (int i = 0; i < meshs_.size(); i++)
+	/*for (int i = 0; i < meshs_.size(); i++)
 	{
 		IRenderMeshObj_SP irmsp;
 		if (first_)
@@ -74,7 +70,7 @@ void DynamicMeshGeoemtry::updateGeometry(const CameraBase* camera)
 		irmsp->clear();
 
 		setupMesh(meshs_[i], irmsp);
-	}
+	}*/
 	computeBoundingBox(NULL);
 	if (first_) first_ = !first_;
 }
@@ -98,33 +94,33 @@ void DynamicMeshGeoemtry::swapQueryTime()
 	if (indicesBuffer_) indicesBuffer_->swapQueryTime();
 }
 
-void DynamicMeshGeoemtry::setupMesh(const Mesh_SP mesh, IRenderMeshObj_SP& obj)
+void DynamicMeshGeoemtry::setupMesh()
 {
+	mesh_obj_->bind();
 
-	obj->mode_ = mesh->mode_;
-	obj->aIndirectCom_ = mesh->aIndirectCom_;
-	obj->call_ = mesh->call_;
-	obj->type_ = mesh->type_;
-	if (obj->type_ == 0) obj->type_ = GL_UNSIGNED_SHORT;
+	uint16 call = mesh_obj_->call();
 
 	Vertex* vertexData = NULL;
-	if (DRAW_ARRAYS == mesh->call_)
+
+	if (DRAW_ARRAYS == call)
 	{
+		MultRenderMeshObj* multRenderObj = (MultRenderMeshObj*)mesh_obj_.addr();
+		assert(multRenderObj);
+		ArraysRenderMeshObj * obj = (ArraysRenderMeshObj*)multRenderObj;
+		assert(obj);
+
 		int vertexSize = 0;
-		const std::vector<std::vector<Vertex> >& vs = mesh->vertices_;
-		int multVertexSize = vs.size();
+		int meshNum = meshs_.size();
 
-		obj->first_ = new int[multVertexSize];
-		obj->count_ = new int[multVertexSize]; //delete
-		obj->drawcount_ = multVertexSize;
+		obj->first_ = new int[meshNum]; //delete
+		obj->count_ = new int[meshNum]; //delete
+		obj->drawcount_ = meshNum;
 
-		for (int i = 0; i < multVertexSize; i++)
+		for (int i = 0; i < meshNum; i++)
 		{
-			vertexSize += obj->count_[i] = vs[i].size();
+			obj->first_[i] = vertexSize;
+			vertexSize += obj->count_[i] = meshs_[i]->vSize();
 		}
-
-		int vnum = vertexSize;
-		int vsize = vnum * sizeof(Vertex);
 
 		if (current_v_index_ + vertexSize > vertexBuffer_->blockSize())
 		{
@@ -137,87 +133,60 @@ void DynamicMeshGeoemtry::setupMesh(const Mesh_SP mesh, IRenderMeshObj_SP& obj)
 		ver_data += current_v_index_;
 
 		//copy from cpu to gpu
-		if (vsize > 0)
+		for (int i = 0; i < meshNum; i++)
 		{
-			for (int i = 0; i < multVertexSize; i++)
-			{
-				const std::vector<Vertex>& vd = vs[i];
-				memcpy(ver_data, &vd[0], obj->count_[i] * sizeof(Vertex));
-				ver_data += obj->count_[i];
-				obj->first_[i] = (i == 0) ? current_v_index_ : obj->first_[i - 1] + obj->count_[ i - 1];
-			}
-
+			memcpy(ver_data, meshs_[i]->cVertex(), meshs_[i]->vSize() * sizeof(Vertex));
+			ver_data += obj->count_[i];
+			obj->first_[i] = (i == 0) ? current_v_index_ : obj->first_[i - 1] + obj->count_[i - 1];
 		}
+
 		vertexBuffer_->setDataSize((vertexSize + current_v_index_) * sizeof(Vertex));
 
 		vertexBuffer_->flush_data();
+
 		if (have_indices_) indicesBuffer_->flush_data();
 
-		obj->vao_ = vao_;
 		obj->pos_vbo_ = vertexBuffer_->VBO();
 
-		current_v_index_ += vnum;
+		current_v_index_ += vertexSize;
 
 	}
-	else if (DRAW_ARRAYS_INDIRECT == mesh->call_)
-	{
-
-	}
-	else if (DRAW_ARRAYS_INSTANC == mesh->call_)
-	{
-
-	}
-	else if (DRAW_ELEMENTS == mesh->call_)
-	{
-		//vertex data
-		const std::vector<std::vector<Vertex> >& vs = mesh->vertices_;
-		int multVertexSize = vs.size();
-		assert(multVertexSize == 1);
-
-		//indices
-		int multIds = mesh->indices_.size();
-		obj->count_ = new int[multIds];  //delete
-		obj->indices_ = (void**)new uint16 * [multIds];//uint16 type->GL_UNSIGNED_SHORT //delete
-		obj->drawcount_ = multIds;
-
-		for (int i = 0; i < multIds; i++)
-		{
-			obj->count_[i] = mesh->indices_[i].size();
-			obj->indices_[i] = new uint16[obj->count_[i]];
-			const std::vector<uint16>& vi = mesh->indices_[i];
-			std::copy(vi.cbegin(), vi.cend(), (uint16*)(obj->indices_[i]));
-		}
-
-		const std::vector<Vertex>& vertexs = vs[0];
-		glGenVertexArrays(1, &obj->vao_);
-		glBindVertexArray(obj->vao_);
-
-		glGenBuffers(1, &obj->pos_vbo_);
-		glBindBuffer(GL_ARRAY_BUFFER, obj->pos_vbo_);
-		glBufferData(GL_ARRAY_BUFFER, vertexs.size() * sizeof(Vertex), &vertexs[0], GL_STATIC_DRAW);
-
-		setupVertexAttribute(mesh->CFVF());
-
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glBindVertexArray(0);
-
-		CHECK_GL_ERROR;
-	}
-	else if (DRAW_ELEMENTS_BASE_VERTEX == mesh->call_) {
-
-	}
-	else if (DRAW_ELEMENT_INSTANCE_BASE_VERTEX == mesh->call_) {
-
-	}
-	else if (DRAW_ELEMENT_INSTANCE_BASE_INSTANCE == mesh->call_) {
-
-	}
-	else if (DRAW_ELEMENT_INDIRECT == mesh->call_) {
-
-	}
-
-
 	
+	else if (DRAW_ELEMENTS == call)
+	{
+		////vertex data
+		//const std::vector<std::vector<Vertex> >& vs = mesh->vertices_;
+		//int multVertexSize = vs.size();
+		//assert(multVertexSize == 1);
+
+		////indices
+		//int multIds = mesh->indices_.size();
+		//obj->count_ = new int[multIds];  //delete
+		//obj->indices_ = (void**)new uint16 * [multIds];//uint16 type->GL_UNSIGNED_SHORT //delete
+		//obj->drawcount_ = multIds;
+
+		//for (int i = 0; i < multIds; i++)
+		//{
+		//	obj->count_[i] = mesh->indices_[i].size();
+		//	obj->indices_[i] = new uint16[obj->count_[i]];
+		//	const std::vector<uint16>& vi = mesh->indices_[i];
+		//	std::copy(vi.cbegin(), vi.cend(), (uint16*)(obj->indices_[i]));
+		//}
+
+		//const std::vector<Vertex>& vertexs = vs[0];
+		//glGenVertexArrays(1, &obj->vao_);
+		//glBindVertexArray(obj->vao_);
+
+		//glGenBuffers(1, &obj->pos_vbo_);
+		//glBindBuffer(GL_ARRAY_BUFFER, obj->pos_vbo_);
+		//glBufferData(GL_ARRAY_BUFFER, vertexs.size() * sizeof(Vertex), &vertexs[0], GL_STATIC_DRAW);
+
+		//setupVertexAttribute(mesh->CFVF());
+
+		//glBindBuffer(GL_ARRAY_BUFFER, 0);
+		//glBindVertexArray(0);
+
+	}
 
 	CHECK_GL_ERROR;
 }
