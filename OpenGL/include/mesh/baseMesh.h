1752548bb56861a3b67d2dc84f0cc75ae1a04b41
千浔 <xtmacbook@users.noreply.github.com>
2021-01-15 16:54:38 +0800

    Update baseMesh.h

--- OpenGL/include/mesh/baseMesh.h
+++ OpenGL/include/mesh/baseMesh.h
@@ -40,23 +40,15 @@ private:
 	float width_, height_, texCoordz_;
 };
 
-class CubGeometry :public MeshGeometry
-{
-public:
-	CubGeometry(const V3f& centerPos, float scale, IRenderMeshObj_SP rsp);
-};
-
 class SphereGeoemtry : public MeshGeometry
 {
 public:
 	typedef Vertex_P Vertex;
+	virtual void computeBoundingBox(void *);
 
-	virtual void computeBoundingBox(void*);
-	SphereGeoemtry(V3f pos, float scale, IRenderMeshObj_SP);
+	SphereGeoemtry(V3f pos,float scale, IRenderMeshObj_SP);
 
-	static Shader_SP	getShader();
 private:
-	static Shader_SP  sphereShader_;
 	V3f pos_;
 	float scale_;
 };
@@ -64,15 +56,22 @@ private:
 class QuadGeometry :public MeshGeometry
 {
 public:
-	QuadGeometry(const V3f& centerPos, float scale, IRenderMeshObj_SP);
+	QuadGeometry(const V3f& centerPos, float scale,IRenderMeshObj_SP);
+};
+
+class CubGeometry :public MeshGeometry
+{
+public:
+	CubGeometry(const V3f& centerPos, float scale, IRenderMeshObj_SP);
 };
 
 LIBENIGHT_EXPORT IRenderNode_SP getLineRenderNode(const V3f& s, const V3f& e, const V3f& color, bool);
 LIBENIGHT_EXPORT IRenderNode_SP getPointsRenderNode(const V3f& s, const V3f& color, bool);
 LIBENIGHT_EXPORT IRenderNode_SP getRayRenderNode(const V3f& s, const V3f& end, const V3f& color, bool);
 LIBENIGHT_EXPORT IRenderNode_SP getHudRenderNode(float xoffset, float yoffset, float width, float height, float texCoordZ, bool);
-LIBENIGHT_EXPORT IRenderNode_SP getQuadRenderNode(const V3f& center = V3f(0.0, 0.0, 0.0), float scale = 1.0, bool u = false);
+LIBENIGHT_EXPORT IRenderNode_SP getQuadRenderNode(const V3f& center = V3f(0.0,0.0,0.0),float scale = 1.0, bool u = false);
 LIBENIGHT_EXPORT IRenderNode_SP getSphereRenderNode(const V3f& center, float scale, bool);
+LIBENIGHT_EXPORT IRenderNode_SP getCubRenderNode(const V3f& center = V3f(0.0, 0.0, 0.0), float scale = 1.0, bool update = false);
 
 
 
