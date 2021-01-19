e79923aeccac268a17537e4d8f2837e94f6345c5
千浔 <xtmacbook@users.noreply.github.com>
2021-01-19 17:21:05 +0800

    Update clipmap.glsl

--- media/shader/test/clipmap.glsl
+++ media/shader/test/clipmap.glsl
@@ -124,7 +124,7 @@ void main()
     else
     {
 		
-		float blendLayers = modf( mipLevel, mipLevel );
+				float blendLayers = modf( mipLevel, mipLevel );
         blendLayers = clamp(blendLayers,0.0,1.0);
         
         float nextMipLevel = mipLevel + 1.0;
@@ -134,12 +134,48 @@ void main()
             
         vec2 clipTexCoord = texCoord / pow( 2, mipLevel );
         clipTexCoord *= g_ScaleFactor;
-        vec4 color1 = texture( StackTexture, vec3( clipTexCoord + 0.5, mipLevel ) );
-            
-        clipTexCoord = texCoord / pow( 2, nextMipLevel );
-        clipTexCoord *= g_ScaleFactor;
-        vec4 color2 = texture( StackTexture, vec3( clipTexCoord + 0.5, nextMipLevel ) );
         
-        color = mix( color0, mix( color1, color2, blendLayers ), blendGlobal ) * diffuse;
+        
+        if(test_ == 1.0) 
+        {
+        	color = texture( StackTexture, vec3( clipTexCoord + 0.5, mipLevel ) );
+        }
+        else if(test_ == 2.0)
+        {
+         	clipTexCoord = texCoord / pow( 2, nextMipLevel );
+        	clipTexCoord *= g_ScaleFactor;
+        	color = texture( StackTexture, vec3( clipTexCoord + 0.5, nextMipLevel ) );
+        }
+       else if(test_ == 3.0)
+       {
+       		vec4 color1 = texture( StackTexture, vec3( clipTexCoord + 0.5, mipLevel ) );
+       		clipTexCoord = texCoord / pow( 2, nextMipLevel );
+        	clipTexCoord *= g_ScaleFactor;
+       		vec4 color2 = texture( StackTexture, vec3( clipTexCoord + 0.5, nextMipLevel ) );
+       		color = mix( color0, mix( color1, color2, blendLayers ), blendGlobal ) * diffuse;
+       }
+       else
+       {
+       	 if(mipLevel == 0.0)
+       	 {
+       	 	 color = vec4(0.0,1.0,0.0,1.0);
+       	 }
+       	 else if(mipLevel == 1.0)
+       	 {
+       	 	color = vec4(0.0,0.0,1.0,1.0);
+       	 }
+       	 else if(mipLevel == 2.0)
+       	 {
+       	 	color = vec4(0.0,1.0,1.0,1.0);
+       	 }
+       	 else if(mipLevel == 3.0)
+       	 {
+       	 	color = vec4(1.0,1.0,1.0,1.0);
+       	 }
+       	 else
+       	 {
+       	 	color = vec4(0.0,0.0,0.0,1.0);
+       	 }
+       }
      }
 }
